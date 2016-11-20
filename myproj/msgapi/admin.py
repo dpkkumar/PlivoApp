2211084364441ad969cf6935eaf59e9c234e7921
dpkkumar <dpkrai58@gmail.com>
2016-11-20 12:51:52 +0530

    Show seconds in datetime fields

--- myproj/msgapi/admin.py
+++ myproj/msgapi/admin.py
@@ -19,10 +19,20 @@ class TaskMetaAdmin(admin.ModelAdmin):
 
 class TaskStatusAdmin(admin.ModelAdmin):
     readonly_fields = ('task_id', 'status', 'created_on', 'updated_on')
-    list_display = readonly_fields
+    list_display = ('task_id', 'status', 'formatted_created_on', 'formatted_updated_on')
 
     def has_add_permission(self, request, obj=None):
         return False
+
+    # For created_on field, also show seconds
+    def formatted_created_on(self, obj):
+        return obj.created_on.strftime("%d %b %Y %H:%M:%S")
+    formatted_created_on.short_description = 'Created At'
+
+    # For created_on field, also show seconds
+    def formatted_updated_on(self, obj):
+        return obj.updated_on.strftime("%d %b %Y %H:%M:%S")
+    formatted_updated_on.short_description = 'Updated At'
 
 
 admin.site.register(TaskMeta, TaskMetaAdmin)
